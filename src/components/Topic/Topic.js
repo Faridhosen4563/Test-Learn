d233e93ae394d5ea7e159c9d544f0a42efb0fd8a
Faridhosen4563 <faridhosen790@gmail.com>
2022-10-11 01:07:08 +0600

    dynamic route create

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Topic = ({ topic }) => {
-    const { name, logo, total } = topic
+    const { id, name, logo, total } = topic
     return (
-        <div className="card w-96 bg-slate-600 border-2 border-black text-white">
+        <div className="my-12 card w-full bg-slate-600 border-2 border-black text-white">
             <figure className="px-10 pt-10">
                 <img src={logo} alt="Shoes" className="rounded-xl" />
             </figure>
@@ -11,7 +12,12 @@ const Topic = ({ topic }) => {
                 <h2 className="card-title">Topic name : {name}</h2>
                 <p>Quiz quantity : {total}</p>
                 <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <Link to={`/${id}`}>
+                        <button className="btn btn-wide text-blue-300 btn-outline">Start Quiz <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                        </svg>
+                        </button>
+                    </Link>
                 </div>
             </div>
         </div>
